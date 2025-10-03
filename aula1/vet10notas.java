40376e3a56a832748628096fd62a626b15a32597
fernandasouzx <fernandasouza.ccomp@gmail.com>
2025-10-03 10:51:14 -0300

    add codigo novo

--- aula1/vet10notas.java
+++ aula1/vet10notas.java
@@ -1,3 +1,13 @@
+/*
+ Programa que armazena em um vetor as notas de 10 alunos.
+ 
+ O programa realiza as seguintes operações:
+   - Lê e guarda as 10 notas digitadas pelo usuário;
+   - Calcula a média aritmética dessas notas;
+   - Exibe a média calculada;
+   - Conta e informa quantas notas ficaram acima da média;
+   - Conta e informa quantas notas ficaram abaixo da média.
+*/
 public class vet10notas {
     public static void main(String[] args) {
         double soma = 0;
@@ -14,16 +24,12 @@ public class vet10notas {
         for(int i=0; i < vet.length; i++){
             if(vet[i] > media){
                 acima = acima + 1;
-               
             }
             if(vet[i]<media){
                 abaixo = abaixo + 1;
-               
             }
-            
         }
-        System.out.println("A qantidade com valores acima da media sao: " + acima);
-        System.out.println("A qauntidade com valores acima da media sao: " + abaixo);
-
+        System.out.println("A quantidade com valores acima da media sao: " + acima);
+        System.out.println("A quantidade com valores acima da media sao: " + abaixo);
     }
 }
